8b5db33c4a1f059af0b2485ad9c942035587cc1a
Matty Kadosh <mattyk@mellanox.com>
2014-01-14 18:45:40 +0200

    add router stucts and enums

--- OES/oes_types.h
+++ OES/oes_types.h
@@ -40,6 +40,9 @@ enum oes_access_cmd {
 	OES_ACCESS_CMD_DISABLE   	= 11,
 	OES_ACCESS_CMD_CREATE    	= 12,
 	OES_ACCESS_CMD_DESTROY   	= 13,
+	OES_ACCESS_CMD_GET       	= 14,
+	OES_ACCESS_CMD_GET_FIRST    = 15,
+	OES_ACCESS_CMD_GET_NEXT     = 16,
 };
 
 enum oes_span_type {
@@ -124,6 +127,23 @@ enum oes_cos_arbitration {
     OES_COS_ARBITRATION_SP ,   /**< SP   arbiter */
 };
 
+enum oes_router_action{
+	OES_ROUTER_ACTION_DROP,
+	OES_ROUTER_ACTION_TRAP,
+	OES_ROUTER_ACTION_MIRROR,
+    OES_ROUTER_ACTION_FORWARD,
+};
+
+enum oes_interface_type {
+	OES_INTERFACE_TYPE_VLAN,
+	OES_INTERFACE_TYPE_ROUTER_PORT,
+};
+
+enum oes_ip_version {
+	OES_IPV4,
+	OES_IPV6,
+};
+
 /************************************************************************************************************/
 /**************************** struct ************************************************************************/
 union oes_span_type_format {
@@ -132,32 +152,32 @@ union oes_span_type_format {
 };
 
 struct  oes_span_remote_eth_l2{
-	unsigned char  tclass;    /**< trafic class */
-	unsigned short vid;      /**< Vlan ID */
-	unsigned char  prio;    /**< Vlan pcp */
-	struct ether_addr	mac; 
+	unsigned char   tclass;    /**< trafic class */
+	unsigned short  vid;      /**< Vlan ID */
+	unsigned char   prio;    /**< Vlan pcp */
+	struct ether_addr   mac; 
 };
 
 struct  oes_span_local{
-	unsigned char  tclass;  /**< trafic class */
+	unsigned char   tclass;  /**< trafic class */
 };
 
 struct oes_span_session_params{
-	oes_span_type_e					    span_type;												    
-	union oes_span_type_format_t		span_type_format;	
+	enum oes_span_type  span_type;												    
+	union oes_span_type_format  span_type_format;	
 };
 
 struct oes_lag_hash_param {
-	oes_lag_hash_type_t lag_hash_type;   /**< Hash type */
-	oes_lag_hash_t lag_hash;             /**< bit field - oes_lag_hash_bit_number_t */
-	oes_lag_seed_t lag_seed;             /**< LAG seed */
+	struct oes_lag_hash_type lag_hash_type;   /**< Hash type */
+	struct oes_lag_hash lag_hash;             /**< bit field - oes_lag_hash_bit_number_t */
+	struct oes_lag_seed lag_seed;             /**< LAG seed */
 };
 
 struct oes_fdb_uc_mac_addr_params {
 	unsigned short   vid;                     /**< Filtering Identifier */
 	struct ether_addr mac_addr;                 /**< MAC address */
     unsigned long log_port;                  /**< Logical port */
-	oes_fdb_mac_entry_type_e entry_type;  /**< FDB Entry Type (dynamic/static)*/
+	enum oes_fdb_mac_entry_type entry_type;  /**< FDB Entry Type (dynamic/static)*/
 };
 
 struct oes_port_speed_capability {
@@ -172,23 +192,127 @@ struct oes_port_speed_capability {
 };
 
 struct oes_port_flow_control_param {
-     oes_flow_control_mode_e   fc_mode;     /**< low control mode  global pause or per prio pause(pfc) */
+     enum oes_flow_control_mode  fc_mode;     /**< low control mode  global pause or per prio pause(pfc) */
      unsigned char prio;                    /**< prio , valid only in pfc mode */
-     oes_port_flow_ctrl_enable_e fc_enable; /**<enable rx, tx or both */
+     enum oes_port_flow_ctrl_enable fc_enable; /**<enable rx, tx or both */
 };
 
 struct oes_cos_tc_ets_config_params {
-	unsigned char tc; /**<! Traffic class ID (0-7) to be configured */
-	unsigned char bw_allocation;	/**<! Bandwidth percentage guaranteed to traffic_class within its TCG. */
-	unsigned int max_bw_value;		/**<! TC shaper configuration Maximal bandwidth allowed for the use traffic_class. */
-	unsigned int max_bw_units;		/**<! TC shaper configuration Maximal bandwidth value units. */
+	unsigned char   tc; /**<! Traffic class ID (0-7) to be configured */
+	unsigned char   bw_allocation;	/**<! Bandwidth percentage guaranteed to traffic_class within its TCG. */
+	unsigned int    max_bw_value;		/**<! TC shaper configuration Maximal bandwidth allowed for the use traffic_class. */
+	unsigned int    max_bw_units;		/**<! TC shaper configuration Maximal bandwidth value units. */
 };
 
 
 struct oes_cos_port_ets_config_params {
-    oes_cos_arbitration_e arbitration;        /**<! select arbitration for port DWRR or SP*/
+    enum oes_cos_arbitration    arbitration;        /**<! select arbitration for port DWRR or SP*/
     unsigned int max_bw_value;		/**<! port global shaper configuration Maximal bandwidth allowed for the use traffic_class. */
 	unsigned int max_bw_units;		/**<! port global shaper configuration Maximal bandwidth value units. */
 };
 
-/**
+struct oes_router_ecmp_hash_fields{
+	unsigned char enable_src_ip;
+    unsigned char enable_dst_ip;
+    unsigned char enable_tc;
+    unsigned char enable_flow_label;
+    unsigned char enable_tcp_udp;
+    unsigned char enable_udp_src_port;
+    unsigned char enable_dst_src_port;
+};
+
+struct oes_router_attributes {
+	unsigned char  enable_ipv4;
+    unsigned char  enable_ipv6;
+	enum oes_router_action ttl_0_action;
+    enum oes_router_action ttl_1_action;
+};
+
+
+struct oes_l3_interface{
+	enum oes_interface_type type;		/**< Router Interface type vlan or router port  */
+	union {
+		struct {
+			int br_id br;		/**< bridge ID */
+			unsigned short vlan;	/**< VLAN ID */
+		} vlan;				/**< VLAN Router Interface */
+		struct {
+			unsigned long port;	/**< Port */
+		} port;			
+	} ifc;					/**< Router Interface parameters */
+};
+
+struct oes_l3_interface_attributes{
+	struct ether_addr	mac_addr;
+	unsigned short	mtu;
+};
+
+struct oes_l3_interface_admin_state {
+	unsigned char  enable_ipv4;
+    unsigned char  enable_ipv6;
+};
+
+
+struct oes_ip_addr {
+	enum oes_ip_version version;
+	union {
+        	struct in_addr ipv4;
+        	struct in6_addr ipv6;
+	} addr;
+};
+
+struct oes_neigh_data  {
+    unsigned int * rif;
+    struct ether_addr * mac_addr; 
+    enum oes_router_action * action;
+	unsigned char * activity;
+};
+ 
+struct oes_ip_prefix {
+	enum oes_ip_version version;
+	struct oes_ip_addr;
+	unsigned int prefix_len;
+};
+
+struct oes_uc_route_data {
+    enum oes_router_action  action;
+    struct oes_ip_addr * next_hop_list;
+    unsigned short   next_hop_cnt;
+};
+
+struct oes_router_cntr {
+	unsigned long long  router_ingress_unicast_packets;
+	unsigned long long  router_ingress_multicast_packets;
+	unsigned long long  router_ingress_unicast_bytes;
+	unsigned long long  router_ingress_multicast_bytes;
+	unsigned long long  router_egress_unicast_packets;
+	unsigned long long  router_egress_multicast_packets;
+	unsigned long long  router_egress_unicast_bytes;
+	unsigned long long  router_egress_multicast_bytes;
+};
+
+
+struct oes_mc_route_key {
+	struct oes_ip_addr *  mc_gruop_ip;
+	struct oes_ip_addr *  sender_ip; /**< sender ip address in oredr to configure *.G rule  sender_ip sould be 0.0.0.0 */
+    unsigned int ingress_rif;
+};
+
+
+struct oes_mc_route_data {
+    struct oes_mc_router_action  action;
+    unsigned int * rif_list,
+    unsigned short rif_cnt,
+};
+
+
+
+
+struct oes_mc_router_action {
+	unsigned char  enable_assert;
+	unsigned char  enable_rpf;
+	enum oes_router_action  action; 
+	unsigned char  dec_ttl;					
+};
+
+
